9c9d6a1a1339b41563744fcba5c207c1601eabac
jeremymshana <dingoamoyi@gmail.com>
2025-06-10 16:48:07 +0300

    q1

--- 04_practical/01/q1.py
+++ 04_practical/01/q1.py
@@ -3,3 +3,14 @@ def reverse_list(lst):
 
 my_list = [1, 2, 3, 4, 5]
 print(reverse_list(my_list))
+#rotating list
+def rotate_list(lst, k):
+    if not lst:
+        return lst
+    k = k % len(lst)  # Handle rotations greater than list length
+    return lst[-k:] + lst[:-k]
+
+
+my_list = [1, 2, 3, 4, 5]
+rotated_list = rotate_list(my_list, 2)
+print(rotated_list)  # Output: [4, 5, 1, 2, 3]
